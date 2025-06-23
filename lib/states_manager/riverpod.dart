0dfca3da7dc928091518ef6878e8417d50f729bc
Kamil Akbarov <kamilaka1979@gmail.com>
2025-06-23 11:10:17 +0300

    Consumer v2

---
+++ lib/states_manager/riverpod.dart
@@ -0,0 +1,10 @@
+// Providers
+// Provider
+// StateProvider
+// StateNotifier & StateNotifierProvider
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final nameProvider = Provider<String>((ref){
+  return 'Hello Kamil!'; 
+});
