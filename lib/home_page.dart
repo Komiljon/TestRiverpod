9a7ad138a5f6e3a650c676e7710dea8c4dbc2514
Kamil Akbarov <kamilaka1979@gmail.com>
2025-06-23 11:34:28 +0300

    StateProvider v1

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,27 +3,30 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:testriverpod/states_manager/riverpod.dart';
 
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key});
+
+  void onSubmit(WidgetRef ref, String value){
+    ref.read(nameStateProvider.notifier).update((state)=>value);
+  }
+
   @override
-  Widget build(BuildContext context) {
-    
+  Widget build(BuildContext context, WidgetRef ref) {
+    final name = ref.watch(nameStateProvider) ?? '';
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter Riverpod'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          children: [   
-          Consumer(builder: 
-            ((context, ref, child){
-              final name = ref.watch(nameProvider);
-              return Text(name);
-            }),
+      body: Column(
+        children: [ 
+          TextField(
+            onSubmitted: (value) {
+              onSubmit(ref, value);
+            },
           ),
-          ],
-        ),
+          Center(child: Text(name)),
+        ],
       ),
     );
   }
--- lib/states_manager/riverpod.dart
+++ lib/states_manager/riverpod.dart
@@ -8,3 +8,5 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final nameProvider = Provider<String>((ref){
   return 'Hello Kamil!'; 
 });
+
+final nameStateProvider = StateProvider<String?>((ref) => null);
